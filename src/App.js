9832b54fcf30d0c30301fd0f05e637b5f5a64eb6
Loop1397 <shwlsdn0906@gmail.com>
2024-06-07 23:48:06 +0900

    Feat: 버튼 제작
    테스트를 위해서 버튼 설치

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 
+const buttonHandler = () => {
+
+}
+
 function App() {
   return (
     <div className="App">
@@ -17,6 +21,7 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={buttonHandler}>버튼</button>
       </header>
     </div>
   );

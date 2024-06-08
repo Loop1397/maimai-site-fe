9a5b4d81ee2ae55f87a891a7d0d29337952f6e54
Loop1397 <shwlsdn0906@gmail.com>
2024-06-08 16:06:50 +0900

    ✨ feat : React Router 적용
    추후 페이지가 여러개 생길 것을 대비해 router를 적용시킴

---
+++ src/pages/MainPage.js
@@ -0,0 +1,12 @@
+const MainPage = () => {
+    return (
+        <div>
+            Hi React!!
+            <button onClick={() => {console.log("button Clicked!!")}}>
+                버튼
+            </button>
+        </div>
+    );
+};
+
+export default MainPage;
